4ba5df95f39888a375aba041fa5b2665a2ea6235
Alexandr D. Borisenko <paz001@yandex.ru>
2016-04-27 12:13:44 +0300

    допилил отправку почты с feedbacka

--- mail.php
+++ mail.php
@@ -1,15 +1,17 @@
 <?php
-$recepient = "paz001@yandex.ru";
+#!/usr/bin/php -q 
+$recepient = "info@tkostrov.spb.ru";
 $sitename = "ОСТРОВ на Индустриальном 19";
 
-$name = trim($_POST["exampleInputName1"]);
-$phone = trim($_POST["exampleInputTel1"]);
-$email = trim($_POST["exampleInputEmail1"]);
-$text = trim($_POST["exampleInputTextArea1"]);
+$name = substr(htmlspecialchars(trim($_POST['Name'])), 0, 100);
+$email = substr(htmlspecialchars(trim($_POST['Email'])), 0, 30);
+$phone = substr(htmlspecialchars(trim($_POST['Tel'])), 0, 30);
+$text = substr(htmlspecialchars(trim($_POST['Text'])), 0, 1500);
 
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$pagetitle = "Новый feedback с сайта \"$sitename\"";
 $message = "Имя: $name \nТелефон: $phone \nEmail: $email \nТекст: $text";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+$head="Content-type:text/plain; \n\t charset=utf-8;"; 
+mail($recepient, $pagetitle, $message, $head);
 if (!headers_sent($filename, $linenum)) {
     header('Location: http://tkostrov.spb.ru/');
     exit;
@@ -19,6 +21,4 @@ if (!headers_sent($filename, $linenum)) {
           "href=\"http://tkostrov.spb.ru/\">Здесь</a> самостоятельно\n";
     exit;
 }
-}
-
 ?>
