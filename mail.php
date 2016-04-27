3954583ffa36f6e7453f265638a8122668a3cc7f
Alexandr D. Borisenko <paz001@yandex.ru>
2016-04-27 10:25:34 +0300

    исправлен mail.php и исправлено меню в футере

--- mail.php
+++ mail.php
@@ -10,12 +10,12 @@ $pagetitle = "Новая заявка с сайта \"$sitename\"";
 $message = "Имя: $name \nТелефон: $phone \nТекст: $text";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
 if (!headers_sent($filename, $linenum)) {
-    header('Location: http://tkostrov.spb.ru/test/index.html');
+    header('Location: http://tkostrov.spb.ru/');
     exit;
 } else {
     echo "Заголовки уже отправлены в $filename на строке $linenum\n" .
           "Редирект невозможен, пожалуйста нажмите <a " .
-          "href=\"http://www.istria-spb.ru\">Здесь</a> самостоятельно\n";
+          "href=\"http://tkostrov.spb.ru/\">Здесь</a> самостоятельно\n";
     exit;
 }
 }
