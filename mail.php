5fdb3012ee458d74141a49f63770360b1fce85af
Alexandr D. Borisenko <paz001@yandex.ru>
2016-04-27 11:43:52 +0300

    Настройка отправки фидбэка

--- mail.php
+++ mail.php
@@ -1,13 +1,14 @@
 <?php
 $recepient = "paz001@yandex.ru";
-$sitename = "ТК ОСТРОВ";
+$sitename = "ОСТРОВ на Индустриальном 19";
 
 $name = trim($_POST["exampleInputName1"]);
 $phone = trim($_POST["exampleInputTel1"]);
+$email = trim($_POST["exampleInputEmail1"]);
 $text = trim($_POST["exampleInputTextArea1"]);
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+$message = "Имя: $name \nТелефон: $phone \nEmail: $email \nТекст: $text";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
 if (!headers_sent($filename, $linenum)) {
     header('Location: http://tkostrov.spb.ru/');
